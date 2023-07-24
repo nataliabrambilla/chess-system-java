b8fa96be89762cf25c8f4aca34278de7110c0d81
nataliabrambilla <nataliabrambilla@hotmail.com>
2023-07-24 14:48:53 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,70 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+//PEÇA - BISPO
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	//IMPRESSÃO DA PEÇA
+	@Override
+	public String toString() {
+		return "B";
+	}
+	
+	//MOVIMENTOS POSSÍVEIS DO BISPO
+	@Override
+	public boolean[][] possibleMoves(){
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		//NOROESTE
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);;
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//NORDESTE
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);;
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//SUDESTE
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);;
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//SUDOESTE
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);;
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+						
+		return mat;
+	}
+}
--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -5,58 +5,70 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
+//PECA - PEÃO
 public class Pawn extends ChessPiece {
 
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
-
+	
+	//IMPRESSÃO DA PEÇA
+	@Override
+	public String toString() {
+		return "P";
+	}
+	
+	//MOVIMENTOS POSSÍVEIS DO PEÃO
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0, 0);
 		
+		//PEÃO BRANCO
 		if (getColor() == Color.WHITE) {
+			//PARA CIMA - UMA POSIÇÃO
 			p.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//PARA CIMA - DUAS POSIÇÕES
 			p.setValues(position.getRow() - 2, position.getColumn());
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//DIAGONAL ESQUERDA - SE HOUVER UMA PEÇA DO ADVERSÁRIO
 			p.setValues(position.getRow() - 1, position.getColumn() - 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//DIAGONAL DIREITA - SE HOUVER UMA PEÇA DO ADVERSÁRIO
 			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 		}
 		
+		//PEÃO PRETO
 		else {
+			//PARA CIMA - UMA POSIÇÃO
 			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//PARA CIMA - DUAS POSIÇÕES
 			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//DIAGONAL ESQUERDA - SE HOUVER UMA PEÇA DO ADVERSÁRIO
 			p.setValues(position.getRow() + 1, position.getColumn() - 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+			//DIAGONAL DIREITA - SE HOUVER UMA PEÇA DO ADVERSÁRIO
 			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
@@ -66,9 +78,6 @@ public class Pawn extends ChessPiece {
 		return mat;
 	}
 	
-	@Override
-	public String toString() {
-		return "P";
-	}
+	
 
 }
